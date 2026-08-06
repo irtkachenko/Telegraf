f34923b93c573567704bbe45edf526e9d5558bd5
devik <devitkachenko@gmail.com>
2026-08-06 23:08:22 +0300

    testing the update system

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -63,7 +63,7 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} ${tektur.variable} ${chakraPetch.variable} antialiased bg-black text-white`}
+        className={`${geistSans.variable} ${geistMono.variable} ${tektur.variable} ${chakraPetch.variable} antialiased bg-red-600 text-white`}
         suppressHydrationWarning
       >
         <PwaRegister />
